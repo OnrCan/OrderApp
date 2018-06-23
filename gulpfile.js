9f8c50c35ea32790b3032d9db391c7119d4eb6a1
Onur Can ÖZKAN <onrcan.ozkan@gmail.com>
2018-06-23 16:46:55 +0300

    front page coding start

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var prefix = require('gulp-autoprefixer');
+var prefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 var nodemon = require('gulp-nodemon');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
-gulp.task('sass', () => {
+gulp.task('sass', function() {
 	return gulp.src('./assets/scss/main.scss')
-		// .pipe(sourcemaps.init())
-		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-		.pipe(prefix())
-		// .pipe(sourcemaps.write())
-		.pipe(gulp.dest('./public/dist/css'));
+			.pipe(plumber([{errorHandler: 'false'}]))
+			.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+			.pipe(prefixer({
+				browsers: ['last 2 versions'],
+				cascade: false
+			}))
+			.pipe(gulp.dest('./public/dist/css/'));
 });
 
 gulp.task('browser-sync', () => {
@@ -23,12 +26,11 @@ gulp.task('browser-sync', () => {
 		port: 5000,
 		notify: false
 	});
-	gulp.watch('./views/*.jade').on('change', reload);
-	gulp.watch('./routes/**/*.js').on('change', reload);
-	gulp.watch('./assets/scss/**/*.scss', ['sass']);
+	gulp.watch('./views/**/*.jade').on('change', reload);
+	gulp.watch('./assets/scss/**/*.scss', ['sass']).on('change', reload);
 });
 
-gulp.task('sass', () => {
+gulp.task('sass:watch', () => {
 	gulp.watch('./assets/scss/**/*.scss', ['sass']);
 })
 
